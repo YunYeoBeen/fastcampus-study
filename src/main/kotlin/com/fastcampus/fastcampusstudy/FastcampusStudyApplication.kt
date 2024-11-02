302b965fa4223a2ac392ef251108d1ceed26f9ca
y0824.yun@samsung.com+2-at-513087882511 <yun-yeobeen@yun-yeobin-ui-MacBookAir.local>
2024-11-02 23:46:10 +0900

    First Commit

---
+++ src/main/kotlin/com/fastcampus/fastcampusstudy/FastcampusStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.fastcampus.fastcampusstudy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FastcampusStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<FastcampusStudyApplication>(*args)
+}
---
+++ src/test/kotlin/com/fastcampus/fastcampusstudy/FastcampusStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fastcampus.fastcampusstudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FastcampusStudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
